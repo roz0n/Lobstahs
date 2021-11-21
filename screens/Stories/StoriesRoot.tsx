d6cc21145cbc6ff1c1b97f9b153f82b720d84306
Arnold Rozon <arnold@rozon.org>
2021-11-20 23:00:11 -0600

    Added stories flatlist

--- screens/Stories/StoriesRoot.tsx
+++ screens/Stories/StoriesRoot.tsx
@@ -1,24 +1,34 @@
 import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { ListRenderItemInfo } from "react-native";
 import { StoriesStackParamsList } from "../../types/storiesStack";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, FlatList, SafeAreaView } from "react-native";
+import { StoriesRootListItem } from "./components/StoriesRootListItem";
+import LIST_MOCK_DATA from "../../mocks/LIST_MOCK_DATA.json";
+
+type StoriesData = {
+  id: string;
+  title: string;
+};
 
 type StoriesRootProps = NativeStackScreenProps<StoriesStackParamsList, "Root">;
 
 export const StoriesRoot = ({ navigation }: StoriesRootProps) => {
-  return (
-    <View style={styles.container}>
-      <Text>Stories Root</Text>
-      <Button title="See details" onPress={() => navigation.navigate("Detail")} />
+  const renderItem = ({ item }: ListRenderItemInfo<StoriesData>) => (
+    <View>
+      <StoriesRootListItem title={item.title} />
     </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList data={LIST_MOCK_DATA} renderItem={renderItem} keyExtractor={(item) => item.id} />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
